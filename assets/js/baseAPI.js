d5d59b08804ef6000766372e4057f6824f099824
fml <2324421470@qq.com>
2021-11-09 22:17:03 +0800

    完成个人中心功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -16,8 +16,8 @@ $.ajaxPrefilter(function(options){
 
     // 全局统一挂载 complete回调函数
     options.complete = function(res){
-        console.log('执行了 complete回调')
-        console.log(res)
+        // console.log('执行了 complete回调')
+        // console.log(res)
         // 在 complete回调函数中，可以使用res.responseJSON 拿到服务器响应回来的数据
         if(res.responseJSON.status === 1 && res.responseJSON.message === 
           '身份认证失败！'){
