f31266321a5196919e12b66afc7f553ffac7df57
fml <2324421470@qq.com>
2021-11-09 09:06:00 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,4 +6,25 @@ $.ajaxPrefilter(function(options){
     // console.log(options.url)
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
     // console.log(options.url)
+
+    // 统一为有权限的接口，设置headers请求
+    if(options.url.indexOf('/my/')!==-1){
+        options.headers={
+            Authorization:localStorage.getItem('token')||''
+        }
+    }
+
+    // 全局统一挂载 complete回调函数
+    options.complete = function(res){
+        console.log('执行了 complete回调')
+        console.log(res)
+        // 在 complete回调函数中，可以使用res.responseJSON 拿到服务器响应回来的数据
+        if(res.responseJSON.status === 1 && res.responseJSON.message === 
+          '身份认证失败！'){
+          // 1、强制清空 token
+          localStorage.removeItem('token')
+          // 2、强制跳转到登录界面
+          location.href='/login.html'
+        }
+    }
 })
--- assets/js/login.js
+++ assets/js/login.js
@@ -40,7 +40,7 @@ $(function(){
         e.preventDefault();
         var data = {username:$('#form_reg [name=username]').val(),password:$('#form_reg [name=password]').val()}
         // 发起ajax的post请求
-        $.post('http://api-breakingnews-web.itheima.net/api/reguser',data,function(res){
+        $.post('/api/reguser',data,function(res){
             if(res.status!==0){
                 return layer.msg(res.message)
             }
